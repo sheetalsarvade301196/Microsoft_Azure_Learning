dfa5540b40cb9ec3b0d444c6decc0a682dc1f345
sheetalsarvade301196 <sheetalsarvade12@gmail.com>
2025-08-28 13:59:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import pyodbc
+import os
 
 app = Flask(__name__)
 
@@ -23,4 +24,6 @@ def dashboard():
     return result
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+
+    port = int(os.environ.get("PORT", 8000))
+   app.run(host='0.0.0.0', port=port)
